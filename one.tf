8e5045d803a6113eb3f02c4aae75ea2d382d0ac1
Antonella Alcaraz <42097832+AntoAlc@users.noreply.github.com>
2021-10-01 13:03:55 -0300

    Update one.tf

--- one.tf
+++ one.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_instance" "backend" {
+resource "aws_instance  backend" {
   ami := data.aws_ami.amazon-linux-2.id
   instance_type = "t2.micro"
   subnet_id = tolist(data.aws_subnet_ids.subnets.ids)[0]
